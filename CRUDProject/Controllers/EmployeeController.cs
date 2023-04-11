a4377277fcf57bad770954a3eed312aa8c2800bf
rhynanxkii11 <125341762+rhynanxkii11@users.noreply.github.com>
2023-04-11 15:22:48 +0800

    Initial commit

---
+++ CRUDProject/Controllers/EmployeeController.cs
@@ -0,0 +1,154 @@
+﻿using CRUDProject.DAL;
+using CRUDProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDAL _dal;
+
+        public EmployeeController(EmployeeDAL dal)
+        {
+            _dal = dal;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>();
+            try
+            {
+                employees = _dal.GetAll();
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+            }
+            return View(employees);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    TempData["errorMessage"] = "Model data is invalid";
+                }
+                bool result = _dal.Insert(model);
+                if (!result)
+                {
+                    TempData["errorMessage"] = "Unable to save the data";
+                    return View();
+                }
+                TempData["successMessage"] = "New data has been added successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                Employee employee = _dal.GetById(id);
+                if (employee.Id == 0)
+                {
+                    TempData["errorMessage"] = $"No data found with an Id: {id}";
+                    return RedirectToAction("Index");
+                }
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee model)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                {
+                    TempData["errorMessage"] = "Model data is invalid";
+                    return View();
+                }
+                bool result = _dal.Update(model);
+
+                if (!result)
+                {
+                    TempData["errorMessage"] = "Unable to update the data";
+                    return View();
+                }
+                TempData["successMessage"] = "Data has been updated successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Employee employee = _dal.GetById(id);
+                if (employee.Id == 0)
+                {
+                    TempData["errorMessage"] = $"No data found with an Id: {id}";
+                    return RedirectToAction("Index");
+                }
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmation(Employee model)
+        {
+            try
+            {
+                bool result = _dal.Delete(model.Id);
+
+                if (!result)
+                {
+                    TempData["errorMessage"] = "Unable to delete the data";
+                    return View();
+                }
+                TempData["successMessage"] = "Data has been deleted successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUDProject/DAL/EmployeeDAL.cs
@@ -0,0 +1,134 @@
+﻿using CRUDProject.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CRUDProject.DAL
+{
+    public class EmployeeDAL
+    {
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+        public static IConfiguration Configuration { get; set; }
+
+        private string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            return Configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<Employee> GetAll()
+        {
+            List<Employee> employeeList = new List<Employee>();
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                //Import System.Data
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "[dbo].[uspGetEmployees]";
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+                while (dr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.Id = Convert.ToInt32(dr["Id"]);
+                    employee.FirstName = dr["FirstName"].ToString();
+                    employee.LastName = dr["LastName"].ToString();
+                    employee.DateOfBirth = Convert.ToDateTime(dr["DateOfBirth"]);
+                    employee.Email = dr["Email"].ToString();
+                    employee.Salary = Convert.ToDouble(dr["Salary"]);
+                    employeeList.Add(employee);
+                }
+                _connection.Close();
+            }
+            return employeeList;
+        }
+        //insert
+        public bool Insert(Employee model)
+        {
+            int id = 0;
+            using(_connection = new SqlConnection(GetConnectionString())) 
+            { 
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "[dbo].[uspInsertGetEmployees]";
+                _command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                _command.Parameters.AddWithValue("@LastName", model.LastName);
+                _command.Parameters.AddWithValue("@DateOfBirth", model.DateOfBirth);
+                _command.Parameters.AddWithValue("@Email", model.Email);
+                _command.Parameters.AddWithValue("@Salary", model.Salary);
+
+                _connection.Open();
+                id = _command.ExecuteNonQuery();
+                _connection.Close();
+            }
+            return id > 0 ? true : false;
+        }
+
+        //update get by Id
+        public Employee GetById(int id)
+        {
+            Employee employee = new Employee();
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                //Import System.Data
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "[dbo].[uspGetEmployeesById]";
+                _command.Parameters.AddWithValue("@Id", id);
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+                while (dr.Read())
+                {
+                    employee.Id = Convert.ToInt32(dr["Id"]);
+                    employee.FirstName = dr["FirstName"].ToString();
+                    employee.LastName = dr["LastName"].ToString();
+                    employee.DateOfBirth = Convert.ToDateTime(dr["DateOfBirth"]);
+                    employee.Email = dr["Email"].ToString();
+                    employee.Salary = Convert.ToDouble(dr["Salary"]);
+
+                }
+                _connection.Close();
+            }
+            return employee;
+        }
+        //Update
+        public bool Update(Employee model)
+        {
+            int deleteRowCount = 0;
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "[dbo].[uspUpdateGetEmployees]";
+                _command.Parameters.AddWithValue("@Id", model.Id);
+                _command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                _command.Parameters.AddWithValue("@LastName", model.LastName);
+                _command.Parameters.AddWithValue("@DateOfBirth", model.DateOfBirth);
+                _command.Parameters.AddWithValue("@Email", model.Email);
+                _command.Parameters.AddWithValue("@Salary", model.Salary);
+
+                _connection.Open();
+                deleteRowCount = _command.ExecuteNonQuery();
+                _connection.Close();
+            }
+            return deleteRowCount > 0 ? true : false;
+        }
+        //Delete
+        public bool Delete(int id)
+        {
+            int idd = 0;
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "[dbo].[uspDeleteGetEmployees]";
+                _command.Parameters.AddWithValue("@Id", id);
+                _connection.Open();
+                idd = _command.ExecuteNonQuery();
+                _connection.Close();
+            }
+            return idd > 0 ? true : false;
+        }
+    }
+}
